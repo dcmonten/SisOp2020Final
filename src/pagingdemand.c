3f45f09c31c7efbf9f91e6e6fb87fe01ce58f691
Daniela C. Montenegro <dc.montenegro.pozo@gmail.com>
2020-09-16 16:50:15 -0500

    memoria principal como arreglo multidimensional de enteros

--- src/pagingdemand.c
+++ src/pagingdemand.c
@@ -23,7 +23,7 @@ typedef struct _Page {
 //list of pages
 LIST_HEAD(list_of_pages, _Page) tabla_de_paginas;
 //list of frames
-
+int memoria_principal[FRAMES][FRAME_SIZE];
 //Imprime la tabla de páginas
 void printPT();
 //Libera la memoria de la tabla de páginas
@@ -31,17 +31,16 @@ void freePT();
 //encuentra la primera página que coincide o la crea y la inserta en la lista
 bool insertOrCreatePage(unsigned int vpn, unsigned int offset);
 void backingStoreData(unsigned int vpn, unsigned int pfn, unsigned int offset);
-
 Page *create_page(unsigned int vpn,unsigned int pfn);
-
-
 unsigned int translate(unsigned int address);
+void inicializar_memoria();
 
 int main(int argc, char *argv[])
 {
-    //Inicialización de listas
+    //Inicialización de tabla de páginas
     LIST_INIT(&tabla_de_paginas);
-    
+    //inicialización de memoria principal
+    inicializar_memoria();
     //Lectura de archivos
     FILE *fp;
     fp  = fopen ("./assets/addresses.txt", "r");
@@ -60,26 +59,44 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+void inicializar_memoria(){
+    for (int i = 0; i < FRAMES; i++)
+    {
+        for (int j = 0; j < FRAME_SIZE; j++)
+        {
+            //Inicializo la memoria principal con ceros
+            memoria_principal[i][j]=0;
+            //el cero representará un espacio vacío de memoria
+        }
+    }
+}
 bool insertOrCreatePage(unsigned int vpn, unsigned int offset){
 
     Page * p;
-
-    backingStoreData(vpn,indice_de_memoria,offset);
+    int requested;
+    unsigned int vaddress = vpn*256+offset;
+    unsigned int paddress;
     LIST_FOREACH(p, &tabla_de_paginas, pptrs) {
         //CASO: HIT
         if(p->vpn==vpn){
             //leer desde frame
+            paddress = p->pfn*256+offset;
+            requested = memoria_principal[p->pfn][offset];
+            printf("\nVirtual index: %u Physical address: %u Value: %d\n",vaddress,paddress,requested);
             return true;
         }  
     }
     //CASO: MISS
     //Cargar proceso desde BACKING_STORE a un frame
-    
+    backingStoreData(vpn,indice_de_memoria,offset);
     //Guardar página en tabla de páginas
     Page * pg = create_page(vpn,indice_de_memoria);
+    requested = memoria_principal[indice_de_memoria][offset];
+    paddress = indice_de_memoria*256+offset;
+    printf("\nVirtual index: %u Physical address: %u Value: %d\n",vaddress,paddress,requested);
     indice_de_memoria++;
     LIST_INSERT_HEAD(&tabla_de_paginas, pg, pptrs);
-
+    
     
     return false;
 }
@@ -87,15 +104,19 @@ void backingStoreData(unsigned int vpn, unsigned int pfn, unsigned int offset){
     //OFFSET DEL BACKING_STORE
     int file_offset = vpn * 256;
     //PROCESO DEL BACKING_STORE
-    unsigned char num[FRAME_SIZE];
+    char num[FRAME_SIZE];
     //UBICAR EL OFFSET
     fseek(backing_store, file_offset, SEEK_SET);
     //LEER 256 CARACTERES DESDE EL OFFSET
     fread(&num, 1, 256, backing_store);
     //reseteo el puntero del archivo
     rewind(backing_store);
-    //Imprimo la data que va al archivo
-    printf("\nVirtual index: %u Physical address: %u Value: %u\n",vpn*256+offset,pfn*256+offset,(int)num[offset]);
+    //Lleno la posición de memoria con la data del frame
+    for (size_t i = 0; i < FRAME_SIZE; i++)
+    {
+        memoria_principal[pfn][i]=(int)num[i];
+    }
+        
 }
 
 unsigned int translate(unsigned int address){
